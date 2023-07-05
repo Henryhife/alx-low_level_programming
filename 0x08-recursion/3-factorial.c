51777c4ece4610ce67a4c7feb51b9b1937f0d919
henryhife <hife55@gmail.com>
2023-07-04 23:31:42 -0700

    recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,23 +3,13 @@
  * factorial - factorial of int
  * @n:int
  * Return:int
- */
+*/
 int factorial(int n)
 {
-	int x;
-
-	if (n == 0)
-	{
-		return (1);
-	}
-	else if (n < 0)
-	{
+	if (n < 0)
 		return (-1);
-	}
+	else if (n < 2)
+		return (n);
 	else
-	{
-		x = n * factorial(n - 1);
-	}
-			return (x);
-
+		return (n * factorial(n - 1));
 }
