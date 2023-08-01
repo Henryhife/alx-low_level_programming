23b69ef92838dc507798f53a1b922bc87e318c57
Achigbue Henry Ifeakachukwu <119707313+Henryhife@users.noreply.github.com>
2023-08-01 14:55:13 +0100

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,28 @@
-s
+#include "lists.h"
+
+/**
+ * listint_len - the function which returns element numbers.
+ *
+ * @h: pointer to the first node
+ *
+ * Return: number of elements
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t elements = 1;
+
+	/*return 0 as no of elements when h is NULL*/
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		/*count all the no of elements*/
+		if (h->n != '\0')
+			elements++;
+
+		h = h->next;
+	}
+
+	return (elements);
+}
