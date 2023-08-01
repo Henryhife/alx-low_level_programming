a91f1dabee5ff5c4c49c9d8709b07eb7da9725e8
Achigbue Henry Ifeakachukwu <119707313+Henryhife@users.noreply.github.com>
2023-08-01 17:31:34 +0100

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,10 @@
-#include "lists.h"
+A#include "lists.h"
 
 /**
  * free_listint2 - write a Function which frees the list and also sets head to NULL
  *
  * @head: pointer to the first node
  *
- * Return: nothing
 */
 void free_listint2(listint_t **head)
 {
@@ -20,6 +19,5 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = temp;
 	}
-
 	*head = NULL;
 }
