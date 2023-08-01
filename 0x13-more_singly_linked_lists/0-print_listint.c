5f2259bd289da5d83062e855b608067bc5b6c4ff
Achigbue Henry Ifeakachukwu <119707313+Henryhife@users.noreply.github.com>
2023-08-01 17:24:19 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 
 /**
- * print_listint - Function whicht prints all the elements of a list
- *
- * @h: pointer to first node
+ * print_listint - fiunction that prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
  *
  * Return: the number of nodes
-*/
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
