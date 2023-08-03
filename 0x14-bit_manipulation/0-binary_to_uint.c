22de2c7d2f9b5d2a5d61b90b57f891b7eb908b73
Achigbue Henry Ifeakachukwu <119707313+Henryhife@users.noreply.github.com>
2023-08-03 20:55:24 +0100

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,29 @@
-s
+#include "main.h"
+
+/**
+ * binary_to_uint - change a binary number to an unsigned int.
+ * @b: pointing to string either of 0 and 1 chars
+ *
+ * Return:converted number, or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int a;
+	unsigned int w;
+
+	w = 0;
+	if (!b)
+		return (0);
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		if (b[a] != '0' && b[a] != '1')
+			return (0);
+	}
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		w <<= 1;
+		if (b[a] == '1')
+			w += 1;
+	}
+	return (w);
+}
