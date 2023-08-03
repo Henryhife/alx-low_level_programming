a71bbc3928afa7e60fbc40a1d4b340c60ba89629
Achigbue Henry Ifeakachukwu <119707313+Henryhife@users.noreply.github.com>
2023-08-03 21:14:18 +0100

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,1 +1,22 @@
-a
+#include "main.h"
+
+/**
+ * set_bit - sets to 1, value of a bit
+ * at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index bit.
+ *
+ * Return: 1 if successful, -1 if it fails
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int m;
+
+	if (index > 63)
+		return (-1);
+
+	m = 1 << index;
+	*n = (*n | m);
+
+	return (1);
+}
